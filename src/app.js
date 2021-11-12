8a8fe036a3988fdc01fd92a93784468115e74c36
salsabil sayed <72264319+salsabilsayed@users.noreply.github.com>
2021-11-13 00:09:44 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const news = require('./tools/newsApi');
+const path = require('path');
+const hbs = require('hbs');
+
+app.set('view engine', 'hbs');
+
+const publicDirectory = path.join(__dirname,'../public');
+app.use(express.static(publicDirectory));
+
+const viewsPath = path.join(__dirname,'../templates/views');
+app.set('views',viewsPath);
+
+const partialsPath = path.join(__dirname,'../templates/partials');
+hbs.registerPartials(partialsPath);
+
+
+app.get('/',(req,res)=>{
+    
+   news((error,data)=>{
+       if(error){
+           return res.send({error})
+       }
+       res.render('index',{
+           newsData: data
+       })
+   });
+})
+
+
+app.get('*',(req,res)=>{
+    res.render('notfound')
+})
+
+app.listen(port,()=>{
+    console.log('server is runnung!');
+})
---
+++ src/tools/newsApi.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+const news = (callback) => {
+   const newsUrl = 'https://newsapi.org/v2/everything?q=keyword&apiKey=c4d50f93836c4c2ea180a1bf448d6cfa';
+
+   request({url:newsUrl,json:true},(error,response)=>{
+       // low level error
+       if(error){
+           callback(error,undefined)
+           //console.log('Error occured!');
+
+       // response error    
+       }else if(response.body.status === 'error'){
+           callback(response.body.code,undefined)
+            //console.log(response.body.code);
+       }
+       else if(response.body.articles.length === 0){
+           callback('invalid parameter',undefined)
+           //console.log('invalid parameter');
+       }else{
+           callback(undefined,response.body.articles);
+          // console.log(response.body.articles);
+       }
+   })
+}
+
+
+
+module.exports = news;
